8500e37c0f361f00bff86db5abf4f7bd96c02e7f
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-06-26 10:29:56 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const api = {
+  endpoint: 'https://api.openweathermap.org/data/2.5/',
+  key: 'aa3e87af4c11d68585d6cd281845d40e'
+};
+
+// To access input field and addEventListener
+const input = document.querySelector('#input');
